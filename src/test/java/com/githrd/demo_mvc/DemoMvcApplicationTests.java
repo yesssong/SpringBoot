bdee0a13cc7318b6602a7afe623da3c9fc3fb184
yesssong <sbgys98@gmail.com>
2024-08-08 10:26:23 +0900

    test

---
+++ src/test/java/com/githrd/demo_mvc/DemoMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.githrd.demo_mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
